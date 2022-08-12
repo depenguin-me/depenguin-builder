c3407ac5cf51676c81db4bce17e780c8e75a7bb8
bretton <bretton@hivemind.net>
2022-08-12 21:15:19 +0200

    adding bsdinstall customisations

---
+++ customfiles/depenguin_bsdinstall.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+set -e
+# shellcheck disable=SC3040
+set -o pipefail
+
+exit_error() {
+    echo "$*" 1>&2
+    exit 1;
+}
+
+# read in variables
+if [ -f depenguin_settings.sh ]; then
+	# shellcheck source=/dev/null
+	. depenguin_settings.sh
+else
+	exit_error "Copy depenguin_settings.sh.sample to depenguin_settings.sh, edit to your needs, then run depenguin_bsdinstall.sh again"
+fi
+
+# check if template installerconfig exists
+if [ ! -f INSTALLERCONFIG.sample ]; then
+	exit_error "Missing INSTALLERCONFIG.sample. Please check location."
+fi
+
+# shellcheck disable=SC3003
+# safe(r) separator for sed
+sep=$'\001'
+
+# change variables in INSTALLERCONFIG to our settings and save to INSTALLERCONFIG.active
+< INSTALLERCONFIG.sample \
+  sed "s${sep}%%hostname%%${sep}$conf_hostname${sep}g" | \
+  sed "s${sep}%%interface%%${sep}$conf_interface${sep}g" | \
+  sed "s${sep}%%ipv4%%${sep}$conf_ipv4${sep}g" | \
+  sed "s${sep}%%ipv6%%${sep}$conf_ipv6${sep}g" | \
+  sed "s${sep}%%gateway%%${sep}$conf_gateway${sep}g" | \
+  sed "s${sep}%%nameserveripv4one%%${sep}$conf_nameserveripv4one${sep}g" | \
+  sed "s${sep}%%nameserveripv4two%%${sep}$conf_nameserveripv4two${sep}g" | \
+  sed "s${sep}%%nameserveripv6one%%${sep}$conf_nameserveripv6one${sep}g" | \
+  sed "s${sep}%%nameserveripv6two%%${sep}$conf_nameserveripv6two${sep}g" | \
+  sed "s${sep}%%username%%${sep}$conf_username${sep}g" | \
+  sed "s${sep}%%pubkeyurl%%${sep}$conf_pubkeyurl${sep}g" | \
+  sed "s${sep}%%disks%%${sep}$conf_disks${sep}g" | \
+  sed "s${sep}%%disktype%%${sep}$conf_disktype${sep}g" \
+  > INSTALLERCONFIG.active
+
+# run installer if enabled or output help text
+if [ "$run_installer" -ne 0 ]; then
+	bsdinstall script ./INSTALLERCONFIG.active
+else
+	echo "Warning: run_installer is not enabled in depenguin_settings.sh"
+	echo ""
+	echo "Run installer manually as follows:"
+	echo ""
+	echo "  bsdinstall script ./INSTALLERCONFIG.active"
+	echo ""
+	exit 0
+fi
---
+++ customfiles/depenguin_settings.sh.sample
@@ -0,0 +1,14 @@
+conf_hostname="your.hostname"
+conf_interface="igb0"
+conf_ipv4=""
+conf_ipv6=""
+conf_gateway=""
+conf_nameserveripv4one=""
+conf_nameserveripv4two=""
+conf_nameserveripv6one=""
+conf_nameserveripv6two=""
+conf_username=""
+conf_pubkeyurl=""
+conf_disks="ada0 ada1"   # or ada0 | or nvme0n1 | or nvme0n1 nvme1n1
+conf_disktype="mirror"   # or stripe for single disk
+run_installer="0"        # set to 1 to enable installer
