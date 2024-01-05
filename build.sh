b1761b39ee44d12c166f90d425a1baf29515e3b6
Michael Gmelin <freebsd@grem.de>
2024-01-05 19:55:04 +0100

    Add build.sh rsync fix

--- build.sh
+++ build.sh
@@ -262,7 +262,7 @@ if [ "$UPLOAD" = "YES" ]; then
 	#removed#
 	#scp "$OUTISO" "$CFG_SSH_REMOTEHOST":"$CFG_SSH_REMOTEPATH"
 	# new approach to deal with bad uploads, make sure host is configed in .ssh/config
-	rsync -P -e ssh "$OUTISO" "$CFG_SSH_REMOTEHOST":"$CFG_SSH_REMOTEPATH"/"$1"
+	rsync -P -e ssh "$OUTISO" "$CFG_SSH_REMOTEHOST":"$CFG_SSH_REMOTEPATH"/.
 fi
 
 # change directory
